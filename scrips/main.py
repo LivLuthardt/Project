1eb5f20b92aff0c046d044a9f020d842cec40632
flipflop2341 <stef.optroodt@gmail.com>
2026-03-12 10:39:19 +0100

    error for gmm

--- scrips/main.py
+++ scrips/main.py
@@ -20,25 +20,29 @@ for r in df.itertuples(index=True):
     x1 = x2 #Set the current point to the past point
 df = df.assign(EllipseXTilt = xtiltAngles, EllipseYTilt = ytiltAngles) #Add the tilt angles as a df column
 df = df.dropna(subset=['dx', 'dy', 'dz'])
-
+"""
 copula_lst = [0 for _ in range(129)]
 for row_n in range(1,129+1):
     data_sorted = sort(df,row_n)
     copula_lst[row_n-1] = bivariate_copula(data_sorted,len(data_sorted))
 print(copula_lst)
-
-fiber_summary_k = perform_kmeans_clustering(fiber_sum,5)
+"""
+#define a number of clusters for all methods
+n=5
+"""
+fiber_summary_k = perform_kmeans_clustering(fiber_sum,n)
 # 2. Merge cluster IDs back to the original points for 3D plotting
 df_clustered_k = df.merge(fiber_sum[['fibre_id', 'cluster_id']], on='fibre_id')
 fig_k = plot_k(df_clustered_k)
-# make a plot of the sse to evaluate the accuracy of the method
-sse_k = sse_plot_k(fiber_sum)
-sse_k.show()
-
+# make a plot of the error
+fig_k_error = sse_plot_k(fiber_sum)
+"""
 # Use GMM clustering instead of KMeans
-fiber_summary_gmm = perform_gmm_clustering(fiber_sum)
+fiber_summary_gmm = perform_gmm_clustering(fiber_sum,n)
 df_clustered_gmm = df.merge(fiber_sum[['fibre_id', 'cluster_id']], on='fibre_id')
 fig_gmm = plot_gmm(df_clustered_gmm)
+# make a plot of the error
+fig_gmm_error = aic_bic_plot_gmm(fiber_sum)
 
 clustered, model = perform_agglomerative_clustering(df)
 fig_agg = plot_agg(clustered)
