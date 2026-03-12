2ea51a26ebb3d768e4537e28ee2b4086d6436e6b
flipflop2341 <stef.optroodt@gmail.com>
2026-03-12 09:52:46 +0100

    SSE plot for kmeans (its bad)

--- scrips/clustering.py
+++ scrips/clustering.py
@@ -1,6 +1,7 @@
 # OVERVIEW OF DIFFERENT METHODS: https://scikit-learn.org/stable/modules/clustering.html
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 import plotly.express as px
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
@@ -8,7 +9,7 @@ from sklearn.preprocessing import StandardScaler
 
 # ---------------------------------------METHOD 1: k means------------------------------------------
      
-def perform_kmeans_clustering(df, n_clusters=5):
+def perform_kmeans_clustering(df, n_clusters):
     features = ['x_mean', 'y_mean', 'angle_x_mean', 'angle_y_mean']
     scaler = StandardScaler()
     scaled_data = scaler.fit_transform(df[features])
@@ -17,8 +18,30 @@ def perform_kmeans_clustering(df, n_clusters=5):
     scaled_df['y_mean'] *= 1.5
     kmeans = KMeans(n_clusters=n_clusters, random_state=42, n_init=10)
     df['cluster_id'] = kmeans.fit_predict(scaled_data)
-    return df
+    return df, kmeans.inertia_
 
+def sse_plot_k(df):
+    sse = []
+    for k in range(1, 11):
+        df , inertia = perform_kmeans_clustering(df,n_clusters=k)
+        sse.append(inertia)
+
+    # Create a DataFrame for plotting
+    plot_df = pd.DataFrame({
+        'Number of Clusters': range(1, 11),
+        'SSE': sse
+    })
+
+    # Create 2D line plot with Plotly Express
+    fig = px.line(
+        plot_df, 
+        x='Number of Clusters', 
+        y='SSE', 
+        markers=True,
+        title="SSE vs Number of Clusters"
+    )
+
+    fig.show()
 
 def plot_k(df_clustered):
     # 3. Plotting (Now df_clustered has x, y, z AND cluster_id)
