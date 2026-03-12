659566d7e4b62dc5bba1b3d5c50b93f4107dd211
melissatraynorrrr <mtraynor@tudelft.nl>
2026-03-12 11:18:47 +0100

    v

--- scrips/main.py
+++ scrips/main.py
@@ -20,6 +20,7 @@ for r in df.itertuples(index=True):
     x1 = x2 #Set the current point to the past point
 df = df.assign(EllipseXTilt = xtiltAngles, EllipseYTilt = ytiltAngles) #Add the tilt angles as a df column
 df = df.dropna(subset=['dx', 'dy', 'dz'])
+print(df)
 """
 copula_lst = [0 for _ in range(129)]
 for row_n in range(1,129+1):
