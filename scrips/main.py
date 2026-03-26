1135e3cf7ff3b01c4291fdfbc708c25e82761ffd
melissatraynorrrr <mtraynor@tudelft.nl>
2026-03-26 10:17:09 +0100

    v

--- scrips/main.py
+++ scrips/main.py
@@ -70,33 +70,14 @@ data_sim_arr = np.empty((len(cop_models),129,n_fibers,2))
 # Generate a list with lists inside it
 cop_lst = [[] for i in range(len(cop_models))]
 
-aic_gaussian = []
-aic_student = []
-aic_frank = []
 for z in zz:
     df_z = sort(df,z,par_1,par_2)
     for i,model in enumerate(cop_models):
-        data_sim_arr[i,z], cop, aic = bivariate_copula(df_z,n_fibers,model=model)
+        data_sim_arr[i,z], cop = bivariate_copula(df_z,n_fibers,model=model)
         cop_lst[i].append(cop)
-        if f"{model}" == 'BicopFamily.gaussian':
-            aic_gaussian.append(aic)
-        elif f"{model}" == 'BicopFamily.student':
-            aic_student.append(aic)
-        elif f"{model}" == 'BicopFamily.frank':
-            aic_frank.append(aic)
-        else:
-            print('aaaa')
     for i in range(len(cop_models)):
         data_sim_arr[i,0] = data_sim_arr[i,1]
 
-gaussian_mean = np.mean(aic_gaussian)
-student_mean = np.mean(aic_student)
-frank_mean = np.mean(aic_frank)
-print(gaussian_mean)
-print(student_mean)
-print(frank_mean)
-
-"""
 sim_arr = reconstruct(data_clean,data_sim_arr[0],zz,n_fibers)
 
 df_columns = ['fibre_id','z','x','y']
@@ -116,7 +97,7 @@ for fibre_id in range(n_fibers):
 
 # Plot synthetic fibers
 
-fig = px.line_3d(sim_df,
+fig = px.line_3d(sim_fibers_df,
                 x="x", y="y", z="z",
                 color="fibre_id",
                 title='Synthetic Fibers')
@@ -143,6 +124,14 @@ plt.close('all')
 ### Plot og and synthetic data
 # plot_og_data(par_1,par_2,mean_arr,df,[67])
 plot_synthetic_data(par_1,par_2,mean_arr,df,data_sim_arr[1],[30])
+
+# ADD THE OTHER COLOUMNS TO SIMM_DF 
+
+# apparently if we dont do this the objects cause everything to break
+sim_df[['x', 'y', 'z']] = sim_df[['x', 'y', 'z']].apply(pd.to_numeric)
+
+sim_df = tangent_angles_central(sim_df)
+sim_fiber_sum, n_sim_fibers = fiber_summary(sim_df)
 
 
 #PCA method figure
@@ -199,4 +188,3 @@ ks_x_list, ks_y_list = ks_by_z_lists(df)
 
 print("KS X:", ks_x_list)
 print("KS Y:", ks_y_list)
-"""
