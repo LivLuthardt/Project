c417cbdee2896c51159f19cbba57946210d8578d
melissatraynorrrr <mtraynor@tudelft.nl>
2026-04-21 10:14:57 +0200

    Update layer_clustering.py

--- scrips/layer_clustering.py
+++ scrips/layer_clustering.py
@@ -1,4 +1,5 @@
 import pandas as pd 
+import numpy as np
 from sklearn.neighbors import NearestNeighbors
 from sklearn.preprocessing import StandardScaler
 from main import data_clean
@@ -9,3 +10,8 @@ layer0 = layer0.reset_index(drop=True)
 features = ['x', 'y', 'angle_x_deg', 'angle_y_deg']
 scaler = StandardScaler()
 scaled_data = scaler.fit_transform(layer0[features])
+
+X= data_clean['z'].to_numpy()
+
+# finding neighbors 
+nbrs = NearestNeighbors(n_neighbors=100, algorithm='auto').fit(X)
