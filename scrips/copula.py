3e4993aa8a745d0ddd10fe5a4e038f2a09a434ad
Hugo Fidder <hugofidder@kpnmail.nl>
2026-04-23 14:50:07 +0200

    Removed some random stuff and added comments Don't conflict

--- scrips/copula.py
+++ scrips/copula.py
@@ -3,21 +3,20 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# np.random.seed(0)  # seed for the random generator
-# n = 1000  # number of observations
-# d = 3  # the dimension
-# mean = 1 + np.random.normal(size=d)  # mean vector
-# cov = np.random.normal(size=(d, d))  # covariance matrix
-# cov = np.dot(cov.transpose(), cov)  # make it non-negative definite
-# x = np.random.multivariate_normal(mean, cov, n)
 
 def sort(data,n,x1='angle_x_deg',x2='angle_y_deg'):
-    # Return tilt angles for a given z-value, first indexing by layer and then by tilt outputs
+    """  
+    Sort pd dataframe by taking 2 parameters for a particular z_idx value
+    """
     return data[data['z_idx'] == n][[x1,x2]].to_numpy()
 
 def bivariate_copula(data,n,model=None): #n is number of fibers in a layer
+    """ 
+    Set up copula from bivariate dataset
+    Returns synthetic dataset and corresponding copula
+    """
+    # Perform PIT on observed data
     u = pv.to_pseudo_obs(data)
-    # pv.pairs_copula_data(u, scatter_size=0.5)
 
     # If no family is specified, this means that cop.select will run to choose a family
     # You should avoid running without specifying a family because it takes ages to run
@@ -28,8 +27,10 @@ def bivariate_copula(data,n,model=None): #n is number of fibers in a layer
         cop = pv.Bicop()
         cop.select(data=u)
 
-
+    # Create synthetic dataset from the fresh copula
     u_sim = cop.simulate(n)
+    
+    # Perform inverse PIT
     data_sim = np.asarray([np.quantile(data[:, i], u_sim[:, i]) for i in range(0, 2)])
     data_sim = np.transpose(data_sim)
 
@@ -45,8 +46,6 @@ def vine_copula(x,n): #n is number of fibers in a layer
     # What does this do??
     pv.pairs_copula_data(u, scatter_size=0.5)
 
-
-    
     u_sim = cop.simulate(n, seeds=[1, 2, 3, 4])
 
     # Reverse pit to get actual values
@@ -56,11 +55,16 @@ def vine_copula(x,n): #n is number of fibers in a layer
 
 
 def plot_cop_parameters(cop_lst,ax1,ax2):
+    """ 
+    Plot copula parameters as a function of Z
+    Put in ax1 and ax2 to be able to plot parameters for different copula collections
+    """
+
     zz = np.arange(len(cop_lst))
     model_lst = [cop.family for cop in cop_lst]
-    model_set = set(model_lst)
 
-    assert len(model_set) == 1, "Can't plot if more than 1 unique family"
+    # Check if all the copulas are of the same family, making it possible to plot them
+    assert len(set(model_lst)) == 1, "Can't plot if more than 1 unique family"
     
     ax1.plot(zz,[cop.parameters[0] for cop in cop_lst],label=model_lst[0])
 
@@ -82,40 +86,23 @@ def get_L_and_phi(df_cleaned):
     df['phi'] = np.arctan2(df['dy'], df['dx'])
     return df
 
-def coordinates(arr, df_clean):
-    df_synthetic = pd.DataFrame(columns=['fibre_id', 'angle_x_deg', 'angle_y_deg', 'x', 'y', 'z'])
-    for i in range(len(arr[0])):
-        for j in range(0,128):
-            if j == 0:
-                df_synthetic.iloc[i*128,3] = df_clean.iloc[i*128,2]
-                df_synthetic.iloc[i*128,4] = df_clean.iloc[i*128,3]
-            df_synthetic.iloc[i*128+j, 0] = i
-            df_synthetic.iloc[i*128+j, 5] = j
-            df_synthetic.iloc[i*128+j, 1] = arr[j][i][0]
-            df_synthetic.iloc[i*128+j, 2] = arr[j][i][1]
-    x_prev = df_synthetic.groupby('fibre_id')['x'].shift(1)
-    y_prev = df_synthetic.groupby('fibre_id')['y'].shift(1)
-    df_synthetic['x'] = x_prev + np.tan(np.radians(df_synthetic['angle_x_deg']))
-    df_synthetic['y'] = y_prev + np.tan(np.radians(df_synthetic['angle_y_deg']))
-
-
-
-
-    return df_synthetic
-
-def reconstruct(df_clean,df_sim,zz,n_fibers):
+def reconstruct(df_clean,df_sim,zz_complete,n_fibers):
+    """ 
+    Reconstruct synthetic fibers from initial starting points and dx and dy arrays 
+    """
     z_scale = 500 / 128
 
-    sim_fibers = np.zeros((len(zz),n_fibers,2))
+    # Create empty array to accomodate new fivers
+    sim_fibers = np.zeros((len(zz_complete),n_fibers,2))
 
+    # Take base layer of measured fibers
     df_0 = sort(df_clean,0,'x','y')
 
+    # Add starting points to each corresponding fiber
     sim_fibers[:,:,:] += df_0
 
-    for i in range(len(zz) - 1):
+    # Cummulatively sum the dx and dy values by converting angles to distance
+    for i in range(len(zz_complete) - 1):
         sim_fibers[i+1:,:,:] += np.tan(np.radians(df_sim[i,:,:])) * z_scale
-
-    # for i in range(len(n_fibers)):
-    #     sim_fibers[]
 
     return sim_fibers
