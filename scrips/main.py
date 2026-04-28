27121cc82966636a44d572eb51b269f3e9fdd677
danielhoepner <dhoepner06@gmail.com>
2026-04-28 09:50:13 +0200

    Update main.py

--- scrips/main.py
+++ scrips/main.py
@@ -38,9 +38,18 @@ plt.savefig(fname="FiniteTiltHex.png")
 # Save and write standard deviations
 fstd = (df[["angle_x_deg"]].std(), df[["angle_y_deg"]].std())
 estd = (df[["EllipseXTilt"]].std(), df[["EllipseYTilt"]].std())
+
+fmean_x = df["angle_x_deg"].mean()
+fmean_y = df["angle_y_deg"].mean()
+
+emean_x = df["EllipseXTilt"].mean()
+emean_y = df["EllipseYTilt"].mean()
 with open("Output.txt", "w") as text_file:
     text_file.write("Finite Difference Standard Deviations (x, y): %s" % str(fstd))
     text_file.write("Ellipse Method Standard Deviations (x, y): %s" % str(estd))
+
+    text_file.write(f"Finite Difference Mean (x, y): {fmean_x}, {fmean_y}\n")
+    text_file.write(f"Ellipse Method Mean (x, y): {emean_x}, {emean_y}\n")
 
 #copulas
 zz = np.arange(1,128)
