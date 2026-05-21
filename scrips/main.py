bdbf7f413a222210261edd31b96485940e16170c
flipflop2341 <stef.optroodt@gmail.com>
2026-05-21 10:04:25 +0200

    more pltos

--- scrips/main.py
+++ scrips/main.py
@@ -24,7 +24,6 @@ plot_ellipse(df,1)
 #Make 2D plot of fiber projection plot
 single_fiber_plot(df,5,'normal')
 single_fiber_plot(raw_df,129,'highly misaligned')
-plot_fibers(raw_df,'pluh')
 
 #Make 1D histograms of ellipsetilt vs angle 
 One_D_ellipse_tilt_hist(df)
